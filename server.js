4a6e5d6130e629066e4de34545cc4f15f7068e8a
Gerson-Felix <gerson.01@outlook.pt>
2024-04-26 20:03:05 +0100

    Criação do Ficheiro routes.js

--- server.js
+++ server.js
@@ -5,27 +5,23 @@
 const http = require('http');
 const debug = require('debug')('nodestr:server');
 const express = require('express');
-const { version } = require('os');
+
+//Importação da Rota
+const routes = require('./routes');
 
 //Criação da app e definição da porta
 const app = express();
 const port = 3000;
 app.set('port', port);
 
+//Execução da Rota
+app.use(routes);
+
 //Criação do Servidor
 const server = http.createServer(app);
-const router = express.Router();
-
-//Rotas
-var route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.1"
-    });
-});
 
 //Execução da app
-app.use('/', route);
+app.use('/', routes);
 
 server.listen(port);
 console.log("Porta da app " + port);
